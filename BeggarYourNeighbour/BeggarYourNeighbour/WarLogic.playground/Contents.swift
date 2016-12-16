f817bab530649339f12037958598c74875c6b6ba
Andrew <aelder@rsgc.on.ca>
2016-12-16 09:15:34 -0500

    Started to work on beggar your neighbour.

--- BeggarYourNeighbour/BeggarYourNeighbour/WarLogic.playground/Contents.swift
+++ BeggarYourNeighbour/BeggarYourNeighbour/WarLogic.playground/Contents.swift
@@ -1,6 +1,42 @@
-//: Playground - noun: a place where people can play
+//
+//  main.swift
+//  WarLogic
+//
+//  Created by Andy-E on 2016-12-12.
+//  Copyright © 2016 Andy-E. All rights reserved.
+//
 
-import Cocoa
+import Foundation
+
+/*
+ func war (comp : Int, play : Int, currentIndex : Int) {
+ 
+ if(comp > play){
+ //remove from other deck and add to computer
+ 
+ for j in 0...3{
+ //puts correct cards in winners deck
+ computerHand.append(computerHand[currentIndex+j])
+ playerHand.remove(at: currentIndex+j)
+ 
+ }
+ print("Computer wins war")
+ } else if(play < comp) {
+ for j in 0...3{
+ //puts the lossers hand in winner
+ playerHand.append(computerHand[currentIndex+j])
+ computerHand.remove(at: currentIndex+j)
+ 
+ }
+ 
+ print("Player wins war")
+ } else {
+ print("another war")
+ 
+ }
+ 
+ }
+ */
 
 // Create an enumeration for the suits of a deck of cards
 enum Suit : String {
@@ -57,14 +93,16 @@ for suit in 0...3 {
 
 // Iterate over the deck of cards
 for card in deck {
-    print("Suit is \(Suit.glyph(forHashValue: card.suit)) and value is \(card.value)")
+    //  print("Suit is \(Suit.glyph(forHashValue: card.suit)) and value is \(card.value)")
 }
 
 // Initialize hands
 var playerHand : [Card] = []
 var computerHand : [Card] = []
 
+
 // "Shuffle" the deck and give half the cards to the player
+
 while deck.count > 26 {
     
     // Generate a random number between 0 and the count of cards still left in the deck
@@ -77,10 +115,122 @@ while deck.count > 26 {
     deck.remove(at: position)
     
 }
+computerHand = deck
 
-// Iterate over the player's hand
-print("=====================================")
-print("All cards in the player's hand are...")
-for (value, card) in playerHand.enumerated() {
-    print("Card \(value) in player's hand is a suit of \(Suit.glyph(forHashValue: card.suit)) and value is \(card.value)")
+/*
+ // Iterate over the player's hand
+ print("=====================================")
+ print("All cards in the player's hand are...")
+ for (value, card) in playerHand.enumerated() {
+ print("Card \(value) in player's hand is a suit of \(Suit.glyph(forHashValue: card.suit)) and value is \(card.value)")
+ }
+ 
+ print("=====================================")
+ print(computerHand)
+ 
+ 
+ 
+ 
+ 
+ */
+//tempHold [] =[]
+
+print("Start with enter key!", terminator:"")
+var cardCount = 0
+var arrayIndex = playerHand.count
+for k in 0...25 {
+    
+    for i in 1...arrayIndex-1 {
+        
+        
+        if playerHand.count > computerHand.count {
+            arrayIndex = computerHand.count
+            print("Cards in computer deck: \(arrayIndex)")
+        } else {
+            arrayIndex = playerHand.count
+            print("Cards in player deck: \(arrayIndex)")
+        }
+        
+        if let input : String = readLine() {
+            
+            if (playerHand.count > i || computerHand.count > i) {
+                var computerCard : Int = computerHand[i].value
+                var playerCard : Int = playerHand[i].value
+                
+                
+                if (computerCard == playerCard){
+                    print("WAR")
+                    
+                    print("Player final war card: ", terminator:"")
+                    print(playerHand[i+3].value, terminator:"")
+                    print(" Computer final war card: ", terminator:"")
+                    print(computerHand[i+3].value)
+                    
+                    
+                    if(computerHand[i+3].value > playerHand[i+3].value){
+                        //remove from other deck and add to computer
+                        
+                        for j in 0...3{
+                            //puts correct cards in winners deck
+                            computerHand.append(computerHand[i+j])
+                            playerHand.remove(at: i+j)
+                            
+                        }
+                        print("Computer wins war")
+                    } else if(computerHand[i+3].value < playerHand[i+3].value) {
+                        for j in 0...3{
+                            //puts the lossers hand in winner
+                            playerHand.append(computerHand[i+j])
+                            computerHand.remove(at: i+j)
+                            
+                        }
+                        
+                        print("Player wins war")
+                    } else {
+                        print("another war")
+                        
+                    }
+                    
+                } else if (computerCard > playerCard) {
+                    
+                    print("Winner: computer")
+                    //remove from other deck and add to computer
+                    computerHand.append(computerHand[i])
+                    playerHand.remove(at: i)
+                    
+                    
+                    cardCount += 1 //setup for next round
+                    
+                } else {
+                    print("Winner: player")
+                    //remove from other deck add to player
+                    playerHand.append(computerHand[i])
+                    computerHand.remove(at: i)
+                    
+                    cardCount += 1 //setup for next round
+                }
+                
+                //trying to make sure i dont get out of index
+                if (playerHand.count > i || computerHand.count > i) {
+                    print("Player Card: ", terminator:"")
+                    print(playerCard, terminator:"")
+                    print(" Computer Card: ", terminator:"")
+                    print(computerCard)
+                    print("Player cards left: ", terminator:"")
+                    print(playerHand.count, terminator:"")
+                    print(" Computer cards left: ", terminator:"")
+                    print(computerHand.count)
+                    
+                } else {
+                    break
+                }
+            }
+        }
+    }
 }
+
+
+
+
+
+
